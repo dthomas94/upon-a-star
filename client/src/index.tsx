65400b55f804601dfddba090f98e90be2d1fc28c
Danielle Thomas <danielle.thomas313@gmail.com>
2020-04-12 14:10:38 -0500

    connect client to graphql

---
+++ client/src/index.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import {
+	ApolloClient,
+	InMemoryCache,
+	NormalizedCacheObject,
+	ApolloProvider,
+} from "@apollo/client";
+import { HttpLink } from "apollo-link-http";
+
+const cache = new InMemoryCache();
+const link = new HttpLink({
+	uri: "http://localhost:4000/",
+}) as any;
+
+const client: ApolloClient<NormalizedCacheObject> = new ApolloClient({
+	cache,
+	link,
+});
+
+ReactDOM.render(
+	<React.StrictMode>
+		<ApolloProvider client={client}>
+			<App />
+		</ApolloProvider>
+	</React.StrictMode>,
+	document.getElementById("root")
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
--- server/src/index.js
+++ server/src/index.js
@@ -11,14 +11,12 @@ const store = createStore();
 // the function that sets up the global context for each resolver, using the req
 const context = async ({ req }) => {
   // simple auth check on every request
-  const auth = (req.headers && req.headers.authorization) || '';
-  const email = new Buffer(auth, 'base64').toString('ascii');
-
-  // if the email isn't formatted validly, return null for user
-  if (!Isemail.validate(email)) return { user: null };
-  // find a user by their email
-  const users = await store.users.findOrCreate({ where: { email } });
-  const user = users && users[0] ? users[0] : null;
+	const token = (req.headers && req.headers.authorization) || '';
+	
+  // find a user by their token
+  const users = await store.users.findOrCreate({ where: { token } });
+	const user = users && users[0] ? users[0] : null;
+	
 
   return { user };
 };
